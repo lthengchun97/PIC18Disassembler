44b6dbe24aee783a5b85ace4eb59d6dea939154d
Lee Theng Chun <lthengchun97@gmail.com>
2017-07-07 09:19:49 +0800

    First try

---
+++ src/disassembler.c
@@ -0,0 +1,7 @@
+#include "disassembler.h"
+
+char *dissamble (uint32_h code)
+{
+  int opcode = code >> 8;
+  opcodeTable[] = {  }
+}
---
+++ test/test_disassembler.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "disassembler.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_disassembler_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement disassembler");
+}
